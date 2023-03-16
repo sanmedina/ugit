d9932302afa379292e7b2f10b3309643e5b337ba
Santiago Lizarazu <santiago.lizarazu@globant.com>
2023-03-16 15:19:27 -0500

    feat: init argument parser

--- src/ugit/cli.py
+++ src/ugit/cli.py
@@ -1,2 +1,22 @@
-def main():
-    print("hello world!")
+import argparse
+
+
+def main() -> None:
+    args = parse_args()
+    args.func(args)
+
+
+def parse_args() -> None:
+    parser = argparse.ArgumentParser()
+
+    commands = parser.add_subparsers(dest="command")
+    commands.required = True
+
+    init_parser = commands.add_parser("init")
+    init_parser.set_defaults(func=init)
+
+    return parser.parse_args()
+
+
+def init(args: argparse.Namespace) -> None:
+    print("Hello, World!")
