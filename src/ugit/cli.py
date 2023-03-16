6f7985dd3bcae8b63179f2e873f1e97e32e3970e
Santiago Lizarazu <santiago.lizarazu@globant.com>
2023-03-16 15:37:04 -0500

    feat: command cat-file

--- src/ugit/cli.py
+++ src/ugit/cli.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import sys
 
 from . import data
 
@@ -22,6 +23,10 @@ def parse_args() -> None:
     hash_object_parser.set_defaults(func=hash_object)
     hash_object_parser.add_argument("file")
 
+    cat_file_parser = commands.add_parser("cat-file")
+    cat_file_parser.set_defaults(func=cat_file)
+    cat_file_parser.add_argument("object")
+
     return parser.parse_args()
 
 
@@ -33,3 +38,8 @@ def init(args: argparse.Namespace) -> None:
 def hash_object(args: argparse.Namespace) -> None:
     with open(args.file, "rb") as f:
         print(data.hash_object(f.read()))
+
+
+def cat_file(args: argparse.Namespace) -> None:
+    sys.stdout.flush()
+    sys.stdout.buffer.write(data.get_object(args.object))
--- src/ugit/data.py
+++ src/ugit/data.py
@@ -4,13 +4,18 @@ import os
 GIT_DIR = ".ugit"
 
 
-def init():
+def init() -> None:
     os.makedirs(GIT_DIR)
     os.makedirs(f"{GIT_DIR}/objects")
 
 
-def hash_object(data: bytes):
+def hash_object(data: bytes) -> str:
     oid = hashlib.sha1(data).hexdigest()
     with open(f"{GIT_DIR}/objects/{oid}", "wb") as out:
         out.write(data)
     return oid
+
+
+def get_object(oid: str) -> bytes:
+    with open(f"{GIT_DIR}/objects/{oid}", "rb") as f:
+        return f.read()
