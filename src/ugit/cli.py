792915da7ac2c7c66f37091fa273d4b439573c12
Santiago Lizarazu <santiago.lizarazu@globant.com>
2023-07-06 15:50:18 -0500

    feat(log): Show refs that point to each commit
    https://www.leshenko.net/p/ugit/#annotate-log

--- src/ugit/cli.py
+++ src/ugit/cli.py
@@ -98,10 +98,15 @@ def commit(args: argparse.Namespace) -> None:
 
 
 def log(args: argparse.Namespace) -> None:
+    refs = {}
+    for refname, ref in data.iter_refs():
+        refs.setdefault(ref.value, []).append(refname)
+
     for oid in base.iter_commits_and_parents({args.oid}):
         commit = base.get_commit(oid)
 
-        print(f"commit {oid}\n")
+        refs_str = f' ({", ".join (refs[oid])})' if oid in refs else ''
+        print(f"commit {oid}{refs_str}\n")
         print(textwrap.indent(commit.message, "    "))
         print()
 
